7fac63e4e38057c0ce4729b62b8eaca4ce53578d
KonradLekawski <Snajperv52@gmail.com>
2017-05-09 12:50:17 +0200

    move updated

--- ButWholeWarmup.py
+++ ButWholeWarmup.py
@@ -2,20 +2,16 @@ from random import randint
 from time import sleep
 import os
 
-def create_board(width, height):
-    board = []
-    for row in range(0,height):
-        board_row = []
-        for column in range(0,width):
-            if row == 0 or row == height-1:
-                board_row.append('X')
-            else:
-                if column == 0 or column == height-1:
-                    board_row.append('X')
-                else:
-                    board_row.append(' ')
-        board.append(board_row)
-    return board
+def create_board(filename="mapa.txt"):
+    with open("mapa.txt","r") as mapa:
+        the_map_list = []
+        for line in mapa.readlines():
+            list_of_chars = []
+            for char in line:
+                if char != "\n":
+                    list_of_chars.append(char)
+            the_map_list.append(list_of_chars)
+    return the_map_list
 
 def getch():
     import sys, tty, termios
@@ -37,37 +33,40 @@ def print_board(board):
         print()
 
 def wsad(board, char, x_pos, y_pos):
-    if char == 's' and  " " in board[x_pos][y_pos +1]:
+    board[x_pos][y_pos] = " "
+    movable_items = [" ","💩","♥"]
+    if char == 'd' and  board[x_pos][y_pos +1] in movable_items:
         y_pos += 1
-    elif char == 'w' and " " in board[x_pos][y_pos - 1]:
+    elif char == 'a' and board[x_pos][y_pos - 1] in movable_items:
         y_pos -= 1
-    elif char == 'a' and " "  in board[x_pos - 1][y_pos]:
+    elif char == 'w' and board[x_pos - 1][y_pos] in movable_items:
         x_pos -= 1
-    elif char == 'd' and " "  in board[x_pos + 1][y_pos]:
+    elif char == 's' and board[x_pos + 1][y_pos] in movable_items:
         x_pos += 1
     elif char == "p":
         exit()
-    return x_pos, y_pos
+    return board, x_pos, y_pos
 
 
 
-def insert_player(board, width, height):
-    board[height][width] = '@'
+def insert_player(board, x_pos, y_pos):
+    board[x_pos][y_pos] = '@'
+
     return board
 
 
 def main():
     char = ''
-    x_pos = 10
-    y_pos = 10
+    x_pos = 1
+    y_pos = 1
+    board = create_board()
     while True:
         char = getch()
-        board = create_board(20, 20)
-        x_pos, y_pos = wsad(board,char, x_pos, y_pos)
+        board,x_pos, y_pos = wsad(board,char, x_pos, y_pos)
         board_with_player = insert_player(board, x_pos, y_pos)
         os.system('clear')
         print_board(board_with_player)
-
-
+        print("y:", x_pos)
+        print("x", y_pos)
 
 main()
