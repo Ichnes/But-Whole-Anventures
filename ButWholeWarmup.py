f90896dcc3cc8d960e21b1a65e99bcd5d3b8bd27
KonradLekawski <Snajperv52@gmail.com>
2017-05-10 14:18:57 +0200

    changes2

--- ButWholeWarmup.py
+++ ButWholeWarmup.py
@@ -1,9 +1,22 @@
 from random import randint
 import os
 import functions
+import time
+class Colour:
+   PURPLE = '\033[95m'
+   CYAN = '\033[96m'
+   DARKCYAN = '\033[36m'
+   BLUE = '\033[94m'
+   GREEN = '\033[92m'
+   YELLOW = '\033[93m'
+   RED = '\033[91m'
+   BOLD = '\033[1m'
+   UNDERLINE = '\033[4m'
+   END = '\033[0m'
+
 
 def create_board(filename="mapa.txt"):
-    with open("mapa.txt","r") as mapa:
+    with open(filename,"r") as mapa:
         the_map_list = []
         for line in mapa.readlines():
             list_of_chars = []
@@ -16,12 +29,18 @@ def create_board(filename="mapa.txt"):
 def print_board(board):
     for row in board:
         for char in row:
+            if char == "♥":
+                char = Colour.RED + "♥" + Colour.END
+            elif char == "💩":
+                char == Colour.GREEN + "💩" + Colour.END
+            elif char == "💣":
+                char == Colour.YELLOW + "💣" + Colour.END
             print(char, end='')
         print()
 
 def wsad(board, char, x_pos, y_pos):
     board[x_pos][y_pos] = " "
-    movable_items = [" ","💩","♥"]
+    movable_items = [" ","💩","♥","💣"]
     if char == 'd' and  board[x_pos][y_pos +1] in movable_items:
         y_pos += 1
     elif char == 'a' and board[x_pos][y_pos - 1] in movable_items:
@@ -41,20 +60,68 @@ def insert_player(board, x_pos, y_pos,player):
     return board
 
 
-def main():
-    char = ''
+def level_1():
+    global inv
+    life = 100
+    char = ""
     x_pos = 1
     y_pos = 1
     board = create_board()
-    functions.delay_print("Fabula.txt")
-    player = functions.character_creation()
-    while True:
+    #functions.delay_print("Fabula.txt")
+    #time.sleep(1)
+    #os.system('clear')
+    while char!= "p":
+        if board[18][34] == player:
+            os.system('clear')
+            return
+        if life <= 0:
+            os.system('clear')
+            print("GAME OVER")
+            return
         char = functions.getch()
         board,x_pos, y_pos = wsad(board,char, x_pos, y_pos)
+        life,inv = functions.interaction_with_items(life,inv,board,x_pos,y_pos)
+
         board_with_player = insert_player(board, x_pos, y_pos,player)
         os.system('clear')
         print_board(board_with_player)
         print("y:", x_pos)
         print("x", y_pos)
+        print(inv, "   życie: " ,life)
 
-main()
+def level_2():
+    global inv
+    life = 100
+    char = ""
+    x_pos = 1
+    y_pos = 1
+    board = create_board("mapa2.txt")
+    #functions.delay_print("Fabula.txt")
+    #time.sleep(1)
+    #os.system('clear')
+    while char!= "p":
+        '''if board[18][34] == player:
+            os.system('clear')
+            return'''
+        if life <= 0:
+            os.system('clear')
+            print("GAME OVER")
+            return
+        char = functions.getch()
+        board,x_pos, y_pos = wsad(board,char, x_pos, y_pos)
+        life,inv = functions.interaction_with_items(life,inv,board,x_pos,y_pos)
+
+        board_with_player = insert_player(board, x_pos, y_pos,player)
+        os.system('clear')
+        print_board(board_with_player)
+        print("y:", x_pos)
+        print("x", y_pos)
+        print(inv, "   życie: " ,life)
+
+
+player = functions.character_creation()
+os.system("clear")
+inv = {"money":0,"poo":0,"secret key":0,""}
+#level_1()
+#os.system('clear')
+level_2()
